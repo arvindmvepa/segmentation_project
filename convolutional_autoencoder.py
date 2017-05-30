b69c3547979957c7e3e5da7ab80b1c8334b6a539
arvindmvepa <arvind.m.vepa@gmail.com>
2017-05-30 14:48:33 -0700

    update to file output images

--- convolutional_autoencoder.py
+++ convolutional_autoencoder.py
@@ -183,16 +183,12 @@ def draw_results(test_inputs, test_targets, test_segmentation, test_accuracy, ne
     fig.suptitle("Accuracy: {}, {}".format(test_accuracy, network.description), fontsize=20)
     for example_i in range(n_examples_to_plot):
         axs[0][example_i].imshow(test_inputs[example_i], cmap='gray')
-        axs[1][example_i].imshow(test_targets[example_i].astype(np.float32), cmap='gray')
-        axs[2][example_i].imshow(
-            np.reshape(test_segmentation[example_i], [network.IMAGE_HEIGHT, network.IMAGE_WIDTH]),
-            cmap='gray')
+        axs[1][example_i].imshow(test_targets[example_i], cmap='gray')
+        axs[2][example_i].imshow(test_segmentation[example_i], cmap='gray')
 
         test_image_thresholded = np.array(
             [0 if x < 0.5 else 255 for x in test_segmentation[example_i].flatten()])
-        axs[3][example_i].imshow(
-            np.reshape(test_image_thresholded, [network.IMAGE_HEIGHT, network.IMAGE_WIDTH]),
-            cmap='gray')
+        axs[3][example_i].imshow(test_image_thresholded, cmap='gray')
 
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
