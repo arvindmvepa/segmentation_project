faa134974ef00a24d8c5b40a5b7a2a2ef7a003ba
arvindmvepa <arvind.m.vepa@gmail.com>
2017-08-27 06:06:14 -0700

    gpu devices check again

--- convolutional_autoencoder.py
+++ convolutional_autoencoder.py
@@ -238,7 +238,7 @@ def draw_results(test_inputs, test_targets, test_segmentation, test_accuracy, ne
 
 def train():
     BATCH_SIZE = 1
-    with tf.device('/gpu:0'):
+    with tf.device('/gpu:1'):
         network = Network()
 
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
@@ -286,7 +286,7 @@ def train():
 
     #config = tf.ConfigProto(device_count = {'GPU': 0,'GPU': 1})
 
-    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
+    with tf.Session(config=tf.ConfigProto(device_count = {'GPU': 0,'GPU': 1}), allow_soft_placement=True, log_device_placement=True)) as sess:
 
         with tf.device('/gpu:1'):
             print(sess.run(tf.initialize_all_variables()))
