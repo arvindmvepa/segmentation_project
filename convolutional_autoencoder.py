b633518d039f24b08e295f93bd9034ba9bc71cac
arvindmvepa <arvind.m.vepa@gmail.com>
2017-08-27 05:45:40 -0700

    trying to see if gpu device placement will work

--- convolutional_autoencoder.py
+++ convolutional_autoencoder.py
@@ -278,9 +278,16 @@ def train():
 
     hooks_binmasks = imgaug.HooksImages(activator=activator_binmasks)
 
+    test_inputs, test_targets = dataset.test_set
+    # test_inputs, test_targets = test_inputs[:100], test_targets[:100]
+    test_inputs = np.reshape(test_inputs, (-1, network.IMAGE_HEIGHT, network.IMAGE_WIDTH, 1))
+    test_targets = np.reshape(test_targets, (-1, network.IMAGE_HEIGHT, network.IMAGE_WIDTH, 1))
+    test_inputs = np.multiply(test_inputs, 1.0 / 255)
+
     #config = tf.ConfigProto(device_count = {'GPU': 0,'GPU': 1})
 
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
+
         with tf.device('/gpu:1'):
             print(sess.run(tf.initialize_all_variables()))
 
@@ -311,14 +318,16 @@ def train():
                 batch_inputs = np.multiply(batch_inputs, 1.0 / 255)
 
                 batch_targets = augmentation_seq_deterministic.augment_images(batch_targets, hooks=hooks_binmasks)
-
-                cost, _ = sess.run([network.cost, network.train_op],
-                                   feed_dict={network.inputs: batch_inputs, network.targets: batch_targets,
-                                              network.is_training: True})
+                with tf.device('/gpu:1'):
+                    cost, _ = sess.run([network.cost, network.train_op], feed_dict={network.inputs: batch_inputs, network.targets: batch_targets, network.is_training: True})
                 end = time.time()
-                print('{}/{}, epoch: {}, cost: {}, batch time: {}'.format(batch_num,
-                                                                          n_epochs * dataset.num_batches_in_epoch(),
-                                                                          epoch_i, cost, end - start))
+                print('{}/{}, epoch: {}, cost: {}, batch time: {}'.format(batch_num, n_epochs * dataset.num_batches_in_epoch(), epoch_i, cost, end - start))
+
+                with tf.device('/gpu:0'):
+                    summary, test_accuracy = sess.run([network.summaries, network.accuracy], feed_dict={network.inputs: test_inputs, network.targets: test_targets, network.is_training: False})
+
+                summary_writer.add_summary(summary, batch_num)
+                print('Step {}, test accuracy: {}'.format(batch_num, test_accuracy))
 
                 if batch_num % 100 == 0 or batch_num == n_epochs * dataset.num_batches_in_epoch():
                     test_inputs, test_targets = dataset.test_set
@@ -329,10 +338,8 @@ def train():
                     test_inputs = np.multiply(test_inputs, 1.0 / 255)
 
                     print(test_inputs.shape)
-                    summary, test_accuracy = sess.run([network.summaries, network.accuracy],
-                                                      feed_dict={network.inputs: test_inputs,
-                                                                 network.targets: test_targets,
-                                                                 network.is_training: False})
+                    with tf.device('/gpu:0'):
+                        summary, test_accuracy = sess.run([network.summaries, network.accuracy], feed_dict={network.inputs: test_inputs, network.targets: test_targets, network.is_training: False})
 
                     summary_writer.add_summary(summary, batch_num)
 
