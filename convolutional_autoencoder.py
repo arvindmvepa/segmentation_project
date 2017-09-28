fe0c47c019c96c2d60dcab0b2852c691bfb76ec5
arvindmvepa <arvind.m.vepa@gmail.com>
2017-09-28 05:44:43 -0700

    thing

--- convolutional_autoencoder.py
+++ convolutional_autoencoder.py
@@ -178,12 +178,15 @@ class Dataset:
                 target1_image = cv2.threshold(target1_image, 127, 1, cv2.THRESH_BINARY)[1]
                 
                 targets.append(target1_image)
-            else:
+
+            elif os.path.exists(target2_image)::
                 
                 target2_image = np.array(skio.imread(target2_image))[:,:,3]
                 target2_image = cv2.threshold(target2_image, 127, 1, cv2.THRESH_BINARY_INV)[1]
                 
                 targets.append(target2_image)
+
+            print("here")
                 
         return inputs, targets
 
