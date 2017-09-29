399cb6f0374a593adcbbfad38f157ea69e169db3
arvindmvepa <arvind.m.vepa@gmail.com>
2017-09-29 08:37:29 -0700

    new commit

--- convolutional_autoencoder.py
+++ convolutional_autoencoder.py
@@ -66,7 +66,7 @@ def dice_coe(output, target, loss_type='jaccard', axis=[1,2,3], smooth=1e-5):
 
 
 def dice_hard_coe(output, target, threshold=0.5, axis=[1,2,3], smooth=1e-5):
-   output = tf.cast(output > threshold, dtype=tf.float32)
+    output = tf.cast(output > threshold, dtype=tf.float32)
     target = tf.cast(target > threshold, dtype=tf.float32)
     inse = tf.reduce_sum(tf.multiply(output, target), axis=axis)
     l = tf.reduce_sum(output, axis=axis)
