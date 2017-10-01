a9ad06af9b147a2a4138ccb4535afab7cf7ec6d2
arvindmvepa <arvind.m.vepa@gmail.com>
2017-09-30 20:13:44 -0700

    updated description

--- conv2d.py
+++ conv2d.py
@@ -102,4 +102,4 @@ class Conv2d(Layer):
         return output
 
     def get_description(self):
-        return "C{},{},{}".format(self.kernel_size, self.output_channels, self.strides[1])
+        return "C{},{},{}".format(self.kernel_size, self.output_channels, self.dilation[1])
