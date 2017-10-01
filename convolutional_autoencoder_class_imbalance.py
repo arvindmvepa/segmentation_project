d7a3a9d427251b0f1a5a23d8404a443103c769d0
arvindmvepa <arvind.m.vepa@gmail.com>
2017-09-30 20:29:49 -0700

    update bug

--- convolutional_autoencoder_class_imbalance.py
+++ convolutional_autoencoder_class_imbalance.py
@@ -234,14 +234,14 @@ class Dataset:
                 # test_image = np.multiply(test_image, 1.0 / 255)
 
                 target1_image = np.array(skio.imread(target1_image))
-                target1_image = cv2.threshold(target1_image, 127, 1, cv2.THRESH_BINARY_INV)[1]
+                target1_image = cv2.threshold(target1_image, 127, 1, cv2.THRESH_BINARY)[1]
                 
                 targets.append(target1_image)
 
             elif os.path.exists(target2_image):
                 
                 target2_image = np.array(skio.imread(target2_image))[:,:,3]
-                target2_image = cv2.threshold(target2_image, 127, 1, cv2.THRESH_BINARY_INV)[1]
+                target2_image = cv2.threshold(target2_image, 127, 1, cv2.THRESH_BINARY)[1]
                 
                 targets.append(target2_image)
             else:
