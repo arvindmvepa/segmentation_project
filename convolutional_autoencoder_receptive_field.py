eda24bee856234af6be6ade5bd0a31eba61244b5
arvindmvepa <arvind.m.vepa@gmail.com>
2017-09-30 17:07:18 -0700

    dilated convolutions test

--- convolutional_autoencoder_receptive_field.py
+++ convolutional_autoencoder_receptive_field.py
@@ -139,8 +139,8 @@ class Network:
 
             layers.append(MaxPool2d(kernel_size=2, name='max_5', skip_connection=True and skip_connections))
 
-            layers.append(Conv2d(kernel_size=7, dilation = 4, output_channels=4096, name='conv_6_1', net_id = net_id))
-            layers.append(Conv2d(kernel_size=1, output_channels=4096, name='conv_6_2', net_id = net_id))
+            layers.append(Conv2d(kernel_size=7, dilation = 3, output_channels=4096, name='conv_6_1', net_id = net_id))
+            layers.append(Conv2d(kernel_size=1, dilation = 3, output_channels=4096, name='conv_6_2', net_id = net_id))
             #layers.append(Conv2d(kernel_size=1, strides=[1, 1, 1, 1], output_channels=1000, name='conv_6_3'))            
 
         self.inputs = tf.placeholder(tf.float32, [None, self.IMAGE_HEIGHT, self.IMAGE_WIDTH, self.IMAGE_CHANNELS],
