813301860bc5b2505d3143f3aa84eb87f0065fc6
arvindmvepa <arvind.m.vepa@gmail.com>
2017-10-01 20:25:54 -0700

    bug

--- kmeans_two_stage.py
+++ kmeans_two_stage.py
@@ -11,6 +11,7 @@ from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
 
+import copy
 from skimage import io
 
 from PIL import Image
@@ -63,8 +64,10 @@ def modify_validation_files(file_indices, min_dist):
         copyfile(input_image, new_path)
 
         file_loc = input_image
-        min_dist = 1024*math.pow(2,.5)
-        
+
+        target1_image = os.path.join('vessels', 'targets1', file)
+        target2_image = os.path.join('vessels', 'targets2', file)
+
         im = io.imread(file_loc)
         imarray = numpy.array(im)
         im_features = np.zeros((imarray.shape[0]*imarray.shape[1],1))
@@ -115,7 +118,14 @@ def modify_validation_files(file_indices, min_dist):
         new_clusters_distance = [dist[2] for dist in new_clusters_centers]
         max_dist_cluster_label = np.argmax(new_clusters_distance)
         
-        if min(new_clusters_distance) > min_dist:    
+        if max(new_clusters_distance) < min_dist:
+
+            if os.path.exists(target1_image):
+                seg_im = io.imread(target1_image)
+                seg_imarray = numpy.array(seg_im)
+            elif os.path.exists(target2_image):
+                seg_im = io.imread(target2_image)
+                seg_imarray = numpy.array(seg_im)[:,:,3]
 
             mean_pixel = np.mean(imarray)
 
@@ -151,7 +161,7 @@ def modify_validation_files(file_indices, min_dist):
             noise_removed += max_dist_cluster_total
             segmented_noise_overall += segmented_noise_total
             net_noise_removed += max_dist_cluster_total-segmented_noise_total
-            io.imsave("vessels/train_inputs_transformed/m"+file, imarray)
+            io.imsave("vessels/val_transformed/m"+file, imarray)
             total_modified_images += 1
 
     return (net_noise_removed, noise_removed, segmented_noise_overall, total_modified_images)
@@ -169,11 +179,13 @@ def two_stage_kmeans(file_indices):
     noise_removed = 0
     segmented_noise_overall = 0
     net_noise_removed = 0
-    min_dist = PriorityQueue()
     image_map = dict()
     segmentation_total = 0
     total_modified_images = 0
     files_list = os.listdir(os.path.join('vessels', 'inputs'))
+
+    min_dist_cand = [1024 * pow(2,.5)]
+    min_dist_thresholds = []
     for file_index in file_indices:
         
         file = files_list[file_index]
@@ -186,7 +198,7 @@ def two_stage_kmeans(file_indices):
         target2_image = os.path.join('vessels', 'targets2', file)
         
         file_loc = input_image
-        min_dist = 1024*math.pow(2,.5)
+        max_dist = 0
         
         im = io.imread(file_loc)
         imarray = numpy.array(im)
@@ -256,6 +268,7 @@ def two_stage_kmeans(file_indices):
         segmented_noise_total = 0
 
         mean_pixel = np.mean(imarray)
+        alt_image = np.copy(imarray)        
 
         for x in range(imarray.shape[0]):
             for y in range(imarray.shape[1]):
@@ -269,6 +282,7 @@ def two_stage_kmeans(file_indices):
                         if seg_imarray[x,y] > 127:
                             segmented_noise_total += 1
                         imarray[x,y]=mean_pixel #or mean
+                        alt_image[x,y] = 255
                     new_image[x,y]=int((cur_label+1)*scale)
                     j+=1
                 else:
@@ -282,10 +296,24 @@ def two_stage_kmeans(file_indices):
             segmented_noise_overall += segmented_noise_total
             net_noise_removed += max_dist_cluster_total-segmented_noise_total
             io.imsave("vessels/train_inputs_transformed/m"+file, imarray)
+            io.imsave("vessels/train_inputs_transformed/a"+file, alt_image)
             total_modified_images += 1
-            #wrong way to do it, fix later
-            if min(new_clusters_distance) < min_dist:
-                min_dist = min(new_clusters_distance)
+            min_dist_cand.append(max(new_clusters_distance))
+        else:
+            min_dist_thresholds.append(max(new_clusters_distance))
+
+        min_dist_cand_copy = copy.copy(min_dist_cand)
+        for threshold in min_dist_thresholds:
+            remove_list =[]
+            for min_dist in min_dist_cand_copy:
+                if min_dist < threshold:
+                   min_dist_cand.remove(min_dist)
+                   remove_list.append(min_dist)
+            for remove_it in remove_list:
+                min_dist_cand_copy.remove(remove_it)
+
+        min_dist = min(min_dist_cand)
+                                
     return (min_dist, net_noise_removed, noise_removed, segmented_noise_overall, total_modified_images, segmentation_total/len(file_indices))
 
 test_clustering()
