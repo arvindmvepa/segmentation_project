afd60dc50d9825a2b98e83523abf60d73b662cb0
arvindmvepa <arvind.m.vepa@gmail.com>
2017-11-25 03:30:13 -0800

    new file

--- convolutional_autoencoder_class_imbalance_new_drive.py
+++ convolutional_autoencoder_class_imbalance_new_drive.py
@@ -239,8 +239,6 @@ class Dataset:
 
             #print(np.array(skio.imread(target_image)).shape)
 
-            
-
             if os.path.exists(target1_image):
 
                 # load grayscale
@@ -391,7 +389,11 @@ def train(train_indices, validation_indices, run_id):
     dataset.test_inputs = test_inputs
     dataset.test_targets = test_targets
 
-    print(train_inputs)
+    print(train_inputs.shape)
+    print(train_targets.shape)
+    print(test_inputs.shape)
+    print(test_targets.shape)
+
     # test_inputs, test_targets = test_inputs[:100], test_targets[:100]
     test_inputs = np.reshape(test_inputs, (-1, 564, 585, 3))
     test_targets = np.reshape(test_targets, (-1, 564, 585, 1))
