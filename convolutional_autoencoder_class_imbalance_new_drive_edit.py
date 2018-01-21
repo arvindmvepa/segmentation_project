f021eb26e330da95d35bf867e5fc8609965dcf40
arvindmvepa <arvind.m.vepa@gmail.com>
2018-01-21 03:42:38 -0800

    test

--- convolutional_autoencoder_class_imbalance_new_drive_edit.py
+++ convolutional_autoencoder_class_imbalance_new_drive_edit.py
@@ -163,7 +163,7 @@ class Network:
             layers.append(Conv2d(kernel_size=7, output_channels=4096, name='conv_6_1', net_id = net_id))
             layers.append(Conv2d(kernel_size=1, output_channels=4096, name='conv_6_2', net_id = net_id))
             #layers.append(Conv2d(kernel_size=1, strides=[1, 1, 1, 1], output_channels=1000, name='conv_6_3'))
-            self.inputs = tf.placeholder(tf.float32, [None, self.INPUT_IMAGE_HEIGHT, self.INPUT_IMAGE_WIDTH, self.IMAGE_CHANNELS],
+            self.inputs = tf.placeholder(tf.float32, [None, INPUT_IMAGE_HEIGHT, INPUT_IMAGE_WIDTH, self.IMAGE_CHANNELS],
                                      name='inputs')
         self.targets = tf.placeholder(tf.float32, [None, self.IMAGE_HEIGHT, self.IMAGE_WIDTH, 1], name='targets')
         self.is_training = tf.placeholder_with_default(False, [], name='is_training')
@@ -192,7 +192,7 @@ class Network:
         for layer in layers:
             net = layer.create_layer_reversed(net, prev_layer=self.layers[layer.name])
 
-        net = tf.image.resize_image_with_crop_or_pad(net,self.IMAGE_HEIGHT,self.IMAGE_WIDTH)
+        net = tf.image.resize_image_with_crop_or_pad(net,IMAGE_HEIGHT,IMAGE_WIDTH)
         self.segmentation_result = tf.sigmoid(net)
 
         # segmentation_as_classes = tf.reshape(self.y, [50 * self.IMAGE_HEIGHT * self.IMAGE_WIDTH, 1])
