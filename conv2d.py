00c0d583a38894f57a3b828c0ca0e91affd7f16c
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-02-12 22:33:15 -0800

    bug

--- conv2d.py
+++ conv2d.py
@@ -43,11 +43,12 @@ class Conv2d(Layer):
 
         return output
 
-    def create_layer_reversed(self, input, prev_layer=None, reuse=False):
+    def create_layer_reversed(self, input, prev_layer=None):
         net_id = self.net_id
         print(net_id)
-
-        with tf.variable_scope('conv', reuse=reuse):
+        print("hi, I printed the correct layer")
+        #make sure to modify
+        with tf.variable_scope('conv', reuse=True):
             W = tf.get_variable('W{}'.format(self.name[-3:]))
             b = tf.Variable(tf.zeros([W.get_shape().as_list()[2]]))
 
@@ -55,37 +56,6 @@ class Conv2d(Layer):
             input, W,
             tf.stack([tf.shape(input)[0], self.input_shape[1], self.input_shape[2], self.input_shape[3]]),
             strides=[1,1,1,1], padding='SAME')
-
-        Conv2d.layer_index += 1
-        output.set_shape([None, self.input_shape[1], self.input_shape[2], self.input_shape[3]])
-
-        output = lrelu(tf.add(tf.contrib.layers.batch_norm(output), b))
-
-        return output
-
-    def create_deconv_layer(self, input):
-        # print('convd2_transposed: input_shape: {}'.format(utils.get_incoming_shape(input)))
-        # W = self.encoder[layer_index]
-        #input_shape = utils.get_incoming_shape(input)
-        #print(input_shape)
-        input_shape = utils.get_incoming_shape(input)
-        print(input_shape)
-        #print("hi")
-        with tf.variable_scope('conv', reuse=False):
-            W = tf.get_variable('W__', shape=(10, 10, 1, 2))
-            b = tf.Variable(tf.zeros([W.get_shape().as_list()[2]]))
-
-        # if self.strides==[1, 1, 1, 1]:
-        #     print('Now')
-        #     output = lrelu(tf.add(
-        #         tf.nn.conv2d(input, W,strides=self.strides, padding='SAME'), b))
-        # else:
-        #     print('1Now1')
-        #print(input_shape)
-        output = tf.nn.conv2d_transpose(
-            input, W,
-            tf.stack([1, self.input_shape[1], self.input_shape[2], self.input_shape[3]]),
-            strides=self.strides, padding='SAME')
 
         Conv2d.layer_index += 1
         output.set_shape([None, self.input_shape[1], self.input_shape[2], self.input_shape[3]])
--- convolutional_autoencoder_class_imbalance_weight_shared.py
+++ convolutional_autoencoder_class_imbalance_weight_shared.py
@@ -176,7 +176,8 @@ class Network:
 
         # DECODER
         for layer in layers:
-            net = layer.create_layer_reversed(net, prev_layer=self.layers[layer.name], reuse=True)
+            #net = layer.create_layer_reversed(net, prev_layer=self.layers[layer.name], reuse=True)
+            net = layer.create_layer_reversed(net, prev_layer=self.layers[layer.name])
 
         self.segmentation_result = tf.sigmoid(net)
 
