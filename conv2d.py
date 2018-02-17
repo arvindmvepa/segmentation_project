1116da9e045564ad2e8f9d50ffcf7d1d4891c168
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-02-16 23:30:03 -0800

    bug

--- conv2d.py
+++ conv2d.py
@@ -43,9 +43,11 @@ class Conv2d(Layer):
         return output
 
     def create_layer_reversed(self, input, prev_layer=None, reuse=False):
+        net_id = self.net_id
+        print(net_id)
 
         with tf.variable_scope('conv', reuse=reuse):
-            W = tf.get_variable('W{}_'.format(self.name[-3:]),
+            W = tf.get_variable('W{}_{}_'.format(self.name[-3:], net_id),
                                 shape=(self.kernel_size, self.kernel_size, self.input_shape[3], self.output_channels))
             b = tf.Variable(tf.zeros([W.get_shape().as_list()[2]]))
 
