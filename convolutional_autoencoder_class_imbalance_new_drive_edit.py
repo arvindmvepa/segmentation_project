0c1f6b4cbdeada05897be80c87a817126f150201
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-03-06 23:32:16 -0800

    bug

--- convolutional_autoencoder_class_imbalance_new_drive_edit.py
+++ convolutional_autoencoder_class_imbalance_new_drive_edit.py
@@ -552,10 +552,10 @@ def train(train_indices, validation_indices, run_id):
                             print(test_inputs[i:(i+1)].shape)
                             print(test_targets[i:(i+1)].shape)
                             print(test_masks[i:(i+1)].shape)
-                            #inputs, masks, results, targets, _, acc = sess.run([network.inputs, network.masks, network.segmentation_result, network.targets, network.summaries, network.accuracy],
-                            # feed_dict={network.inputs: test_inputs[i:(i+1)], network.masks:  test_masks[i:(i+1)], network.targets: test_targets[i:(i+1)], network.is_training: False})
                             inputs, masks, results, targets, _, acc = sess.run([network.inputs, network.masks, network.segmentation_result, network.targets, network.summaries, network.accuracy],
-                                                                               feed_dict={network.inputs: test_inputs[i:(i+1)], network.targets: test_targets[i:(i+1)], network.is_training: False})
+                                                                               feed_dict={network.inputs: test_inputs[i:(i+1)], network.masks: test_masks[i:(i+1)], network.targets: test_targets[i:(i+1)], network.is_training: False})
+                            #inputs, masks, results, targets, _, acc = sess.run([network.inputs, network.masks, network.segmentation_result, network.targets, network.summaries, network.accuracy],
+                                                                               #feed_dict={network.inputs: test_inputs[i:(i+1)], network.targets: test_targets[i:(i+1)], network.is_training: False})
                             inputs = inputs[0,:,:,0]
                             masks = masks[0, :, :, 0]
                             results = results[0,:,:,0]
